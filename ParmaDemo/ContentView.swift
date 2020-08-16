8e06029c2af57e8e52c5b3ed25234d77197d7501
Leonard Chan <wxclx98@gmail.com>
2020-08-16 21:46:30 +0800

    Initial Commit

---
+++ ParmaDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ParmaDemo
+//
+//  Created by Leonard Chen on 8/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ParmaDemo/ParmaDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParmaDemoApp.swift
+//  ParmaDemo
+//
+//  Created by Leonard Chen on 8/16/20.
+//
+
+import SwiftUI
+
+@main
+struct ParmaDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
